f0f43a4a97451268569f49b158cc92f6e6189376
Helicoir <ga.helicoir@gmail.com>
2023-05-11 19:02:24 +0900

    テスト通るように

--- src/configs/API/endpoints.ts
+++ src/configs/API/endpoints.ts
@@ -3,7 +3,7 @@ import { ValueOf } from "@/utils/types";
 const baseUrl = "/api";
 
 export const APIEndpoints = {
-  TEST_ENDPOINT: `${baseUrl}/test`,
+  TEST_ENDPOINT: `${baseUrl}/sample`,
   TEST_HELLO_ENDPOINT: `${baseUrl}/hello`,
   TEST_COORDINATE_ENDPOINT: `${baseUrl}/coordinates`,
 };
